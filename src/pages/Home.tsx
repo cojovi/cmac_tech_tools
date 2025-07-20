e3e6b238bf4faa0153ef7b0c349df44e59d65aa7
cojovi <31863818+cojovi@users.noreply.github.com>
2025-07-20 17:57:37 -0500

    Remove testimonials section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../components/home/Hero';
 import ServicesPreview from '../components/home/ServicesPreview';
 import ProjectsGallery from '../components/home/ProjectsGallery';
-import Testimonials from '../components/home/Testimonials';
 import CTASection from '../components/home/CTASection';
 
 const Home: React.FC = () => {
@@ -11,7 +10,6 @@ const Home: React.FC = () => {
       <Hero />
       <ServicesPreview />
       <ProjectsGallery />
-      <Testimonials />
       <CTASection />
     </div>
   );
